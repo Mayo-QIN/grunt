dfa196eef163a8f01ec7b128a5e3c5618c4b6793
Korfiatis, Panagiotis, Ph.D <Korfiatis.Panagiotis@mayo.edu>
2016-03-25 10:25:09 -0500

    Add feature selection demo added

---
+++ pipelines/featsel_demo.py
@@ -0,0 +1,17 @@
+"""
+feature selection demo
+
+"""
+
+from _grunt import *
+
+g = grunt("http://ril-gpu10:9916")
+# Syntax 1
+j=g.featsel
+j.datset="/Users/m112447/Documents/TestData/diab.csv"
+j.output="featsel"
+job =j()
+job.wait()
+# Write some output
+job.save_output("output", "/Users/m112447/Downloads/")
+
