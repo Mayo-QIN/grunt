0b2d716ee94166f9678f5d82dd9a301b262e58a6
Daniel Blezek <blezek.daniel@mayo.edu>
2017-04-11 11:41:53 -0500

    Bumping version

--- version.go
+++ version.go
@@ -8,9 +8,9 @@ var VersionInfo = struct {
 	Date        string
 }{
   "1.0.1",
-  "1.0.1",
-  "496d9e17a5851b3dda46d2c773c68c5709809b42",
-  "496d9e1",
-  "2017-03-30-1628 UTC",
+  "1.0.1-3-g80c6b4d",
+  "80c6b4d829987157ab2b0e9de248cafc36c61cdf",
+  "80c6b4d",
+  "2017-04-11-1640 UTC",
 }
 
