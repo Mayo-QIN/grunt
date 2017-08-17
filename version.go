7d175b4ae5d4b378d2507017333e60a5c1b2d2f5
Daniel Blezek <blezek.daniel@mayo.edu>
2017-08-17 12:44:27 -0500

    Updated version info

--- version.go
+++ version.go
@@ -1,8 +1,8 @@
 package main
 
 
-var Version string = "1.0.1"
-var FullVersion string = "1.0.1-24-g68d5223"
-var BuildTimestamp string = "2017-08-17-1717 UTC"
-var Hash string = "68d52234a518aa25b803cd182a9eb7fb94ea9afc"
+var Version string = "1.0.2"
+var FullVersion string = "1.0.2"
+var BuildTimestamp string = "2017-08-17-1744 UTC"
+var Hash string = "7fe30d4360d9aa57693758637df3da9b82388aba"
 
