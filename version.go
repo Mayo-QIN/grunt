e9b57217700c765de1270df099072a1e71500d96
Daniel Blezek <blezek.daniel@mayo.edu>
2017-08-17 12:21:20 -0500

    Static version information

---
+++ version.go
@@ -0,0 +1,8 @@
+package main
+
+
+var Version string = "1.0.1"
+var FullVersion string = "1.0.1-24-g68d5223"
+var BuildTimestamp string = "2017-08-17-1717 UTC"
+var Hash string = "68d52234a518aa25b803cd182a9eb7fb94ea9afc"
+
