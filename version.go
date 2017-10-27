7daf042d3266f6c97c3be7c9709fdcbf6c7c7f1d
Korfiatis <m112447@r5013300.mayo.edu>
2017-10-27 09:24:06 -0500

    Modify service template, change wording and run make to get version

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package main
 
 
 var Version string = "1.0.1"
-var FullVersion string = "1.0.1-49-g40daad4"
-var BuildTimestamp string = "2017-10-19-1428 UTC"
-var Hash string = "40daad47628be534bf62fec275fabfbca23d97ce"
+var FullVersion string = "1.0.1-59-g81ab978"
+var BuildTimestamp string = "2017-10-27-1420 UTC"
+var Hash string = "81ab97861d6a5f5149e993ce166fde9304d2f0fc"
 
